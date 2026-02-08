0e5ece221c472cd0a6f3697a161a29811da9364a
bombel <79379584+b0mbeI@users.noreply.github.com>
2026-02-08 12:06:07 +0100

    Delete gamgob directory

--- gamgob/results.py
+++
@@ -1,6 +0,0 @@
-def print_results(word, status, size, duration):
-    status_str = status
-    size_str = f"{size} bytes"
-    duration_str = f"{duration*1000:.0f} ms"
-    print(f"{word:<25}[Status: {status_str}, Size: {size_str}, Duration: {duration_str}]")
-    
