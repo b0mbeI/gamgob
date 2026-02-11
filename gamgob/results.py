e5c7c866d5e7eb4b3bd8572f9005ae466ff0c873
szczepan <79379584+szc-sw@users.noreply.github.com>
2026-02-11 20:15:41 +0100

    add HTTP marker variable

--- gamgob/results.py
+++ gamgob/results.py
@@ -1,6 +1,7 @@
 def print_results(word, status, size, duration):
     status_str = status
+    marker = " [+]" if status == 200 else ""
     size_str = f"{size} bytes"
     duration_str = f"{duration*1000:.0f} ms"
-    print(f"{word:<25}[Status: {status_str}, Size: {size_str}, Duration: {duration_str}]")
+    print(f"{word:<25}[Status: {status_str}, Size: {size_str}, Duration: {duration_str}]{marker}")
     
