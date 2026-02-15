93ca1eb8307cf0576898b763d5409fdc810d6bff
szczepan <79379584+szc-sw@users.noreply.github.com>
2026-02-15 16:18:56 +0100

    definied json_output function for file saving arg

--- gamgob/results.py
+++ gamgob/results.py
@@ -1,3 +1,6 @@
+import json
+from datetime import datetime
+
 def print_results(word, status, size, duration):
     status_str = status
     marker = " [+]" if status == 200 else ""
@@ -5,3 +8,10 @@ def print_results(word, status, size, duration):
     duration_str = f"{duration*1000:.0f} ms"
     print(f"{word:<25}[Status: {status_str}, Size: {size_str}, Duration: {duration_str}]{marker}")
     
+def json_output(filename, results):
+    date = datetime.now().strftime("%Y-%m-%d")
+    jsonfile = (f"{filename}_{date}.json")
+    with open(jsonfile, "w", encoding="utf-8") as f:
+        json.dump(results, f, indent=4)
+
+    print(f"Results saved to {jsonfile}")
