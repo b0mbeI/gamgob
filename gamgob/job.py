d5ed4e9f7eedbe949e506bad8f48c8f615616264
szczepan <79379584+szc-sw@users.noreply.github.com>
2026-02-15 16:19:40 +0100

    add json_output for file saving in json

--- gamgob/job.py
+++ gamgob/job.py
@@ -4,7 +4,7 @@ import sys
 from urllib.parse import urljoin
 from gamgob.config import logo
 from gamgob.input import load_wordlist
-from gamgob.results import print_results
+from gamgob.results import print_results, json_output
 from gamgob.request import http_check
 
 def run():
@@ -27,6 +27,16 @@ def run():
         default = 1,
         help="delay between requests in seconds"
         )
+    parser.add_argument(
+        "-sv", "--save",
+        required=False,
+        help="save results to JSON file"
+        )
+    parser.add_argument(
+        "-fr", "--filter",
+        type = int,
+        help="show ony specific HTTP status code (e.g. 200)"
+    )
     
     args = parser.parse_args()  
     time.sleep(0.5)
@@ -42,8 +52,10 @@ def run():
     words = load_wordlist(args.wordlist)
     total_words = len(words)
     total_duration = 0
+    filename = args.save
     base_url = args.url
     http_sum = 0
+    results = []
 
     if not base_url.startswith(("http://", "https://")):
         base_url = "https://" + base_url
@@ -53,18 +65,29 @@ def run():
     for i, word in enumerate(words, start=1):
         target = urljoin(base_url, word)
         status, size, duration = http_check(target)
+        if args.filter and status != args.filter:
+            continue
         total_duration += duration
         if status == 200:
             http_sum += 1
-    
+        results.append({
+             "word": word,
+             "url": target,
+             "status": status,
+             "size": size,
+          })      
         sys.stdout.write("\r\033[2K")
+        
         print_results(word, status, size, duration)
+        
         progress_line = f"Progress: {i}/{total_words} | Duration: {total_duration:.2f}s"
+      
         sys.stdout.write(progress_line)
         sys.stdout.flush()
+   
         if args.delay > 0:
             time.sleep(args.delay)
-    
+   
     sys.stdout.write("\r\033[2K")
     
     print(f"\n" + "-"*45)
@@ -73,4 +96,5 @@ def run():
     print(f"[ ] HTTP 200 found : {http_sum}/{total_words}")
     print(f"[ ] Total duration : {total_duration:.2f}s")
     print()
-
+    if args.save:
+        json_output(filename, results)
